87ec22b72d13d50dbbb0583aa1d733be3a1b129d
Emre Çanakçı <83945108+emreosminho@users.noreply.github.com>
2024-01-01 12:08:57 +0300

    Create a Working Contact Form
    I have shown you how to Create A working Contact Form in HTML CSS JavaScript & PHP. Contact Form is an element on a page that allows users to communicate with the website owner. This form has some fields for filling name, email address, subject, message, etc.

---
+++ message.php
@@ -0,0 +1,30 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $website = $_POST['website'];
+    $message = $_POST['message'];
+
+
+    if(!empty($email) && !empty($message)){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $receiver = "emreosmancanakci@gmail.com";
+            $subject = "Form: $name <$email>";
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message\n\nRegards, \n$name";
+            $sender = "From: $email";
+            if(mail($receiver, $subject, $body, $sender)){
+                echo "Your message has been sent";
+            }else{
+                echo "Sorry, faild to send your message!";
+            }
+        }else{
+            echo "Enter a vaild email address!";
+        }
+    }else{
+        echo "Email and password field is required!";
+    }
+
+
+
+
+?>
